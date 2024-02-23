d2c385de7bf1d9ad66796a1f130be3efbd5abd84
Dan Trinh <149278021+nasamuse@users.noreply.github.com>
2024-02-22 19:43:10 -0500

    Background color with camera

--- 4714C Final Project/Assets/Scripts/Create Map and Type.cs
+++ 4714C Final Project/Assets/Scripts/Create Map and Type.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateMapandType : MonoBehaviour
 {
     //The int will be measured as pixels
-    public int lengthOfMap = 10;
-    public int heightOfMap = 10;
+    //public int lengthOfMap = 10;
+    //public int heightOfMap = 10;
 
-    
-    private Rect map;
+    private Camera cam;
+    private Color backgroundColor;
     private string typeSelection;
+    
+
     public enum levelTypes
     {
         Grassland,
@@ -26,9 +29,11 @@ public class CreateMapandType : MonoBehaviour
     //Depending on the enum selected, make the map with length, height, and color it here through if statements or cases
     void Start()
     {
+        cam = GetComponent<Camera>();
         chosenLevel = levelSelect;
         typeSelection = chosenLevel.ToString();
         Debug.Log(typeSelection);
+        
         FindLevelType(typeSelection);
     }
 
@@ -36,13 +41,18 @@ public class CreateMapandType : MonoBehaviour
     {
         switch(typeSelection)
         {
+            //instantiate the map here
+            //Actually gives the background color to main camera to simulate a map
             case ("Grassland"):
+                cam.backgroundColor = new Color(0f, 0.55f, 0.2f, 1f);
                 Debug.Log("Make Grass");
                 break;
             case ("Desert"):
+                cam.backgroundColor = new Color(0.8f, 0.8f, 0.5f, 1f);
                 Debug.Log("Make Desert");
                 break;
             case ("Snow"):
+                cam.backgroundColor = new Color(0.8f, 0.95f, 0.95f, 1f);
                 Debug.Log("Make Snow");
                 break;
         }
