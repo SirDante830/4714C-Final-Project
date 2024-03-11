12c5ee8236e91ab07345822fdb8fb61ed4cc52b3
ALabroo18 <102484252+ALabroo18@users.noreply.github.com>
2024-03-10 23:50:01 -0400

    Updated EnemyScript
    Enemy can be destroyed based on their HP

--- 4714C Final Project/Assets/Scripts/EnemyScript.cs
+++ 4714C Final Project/Assets/Scripts/EnemyScript.cs
@@ -11,6 +11,7 @@ public class EnemyScript : MonoBehaviour
     private Color classColor;
     private string className;
     private string EnemySelection;
+    [SerializeField] private GameObject enemy;
     public enum EnemyTypes
     {
         Crawler,
@@ -43,23 +44,29 @@ public class EnemyScript : MonoBehaviour
                 break;
             case "Speeder":
                 EnemyClass(EnemySelection);
-                EnemyHP = 100;
+                EnemyHP = 150;
                 EnemySpeed = 2.5f;
                 break;
             case "Shooter":
                 EnemyClass(EnemySelection);
-                EnemyHP = 100;
+                EnemyHP = 200;
                 EnemySpeed = 1.5f;
                 break;
             default:
                 break;
         }
     }
-    private void OnColliderEnter2D(Collider Hit)
+    private void OnTriggerEnter2D(Collider2D Hit)
     {
-        if (Hit.CompareTag("Player"))
+        if (Hit.CompareTag("Arrow"))
         {
+            Debug.Log("hit");
             EnemyHP -= 50;
+        }
+        if(EnemyHP <= 0)
+        {
+            Debug.Log("Destroyed");
+            Destroy(enemy);
         }
     }
 }
