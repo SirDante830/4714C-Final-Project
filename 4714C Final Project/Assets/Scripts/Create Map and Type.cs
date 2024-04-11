c32cf176d2267a1aa4d5eb6817433c73d86e3999
Dormio-san <liam.lovett06@gmail.com>
2024-04-11 18:10:49 -0400

    Create main menu and different maps when biome is chosen.
    Main menu that can be operated with keyboard, mouse, or controller. Choosen a biome and enjoy!
    Each biome button chooses a random map size and obstacle amount from the ranges provided in the code.

--- 4714C Final Project/Assets/Scripts/Create Map and Type.cs
+++ 4714C Final Project/Assets/Scripts/Create Map and Type.cs
@@ -10,11 +10,11 @@ public class CreateMapandType : MonoBehaviour
     //The int will be measured as pixels
     //public int lengthOfMap = 10;
     //public int heightOfMap = 10;
-    public int obstaclesToSpawn = 10;
+    public static int obstaclesToSpawn;
     private Camera cam;
     private Color backgroundColor;
     private string typeSelection;
-    public float mapSize;
+    public static float mapSize;
     private Color mapColor;
 
     public enum levelTypes
@@ -27,13 +27,13 @@ public class CreateMapandType : MonoBehaviour
     //Allows the user to select their level type in the Unity Editor
     [SerializeField] public levelTypes levelSelect;
     //This stores the level selected, and it will be used in the switch case statement to establish the map
-    private levelTypes chosenLevel;
+    [HideInInspector] public static levelTypes chosenLevel;
 
     //Depending on the enum selected, make the map with length, height, and color it here through if statements or cases
     void Start()
     {
         cam = GetComponent<Camera>();
-        chosenLevel = levelSelect;
+        //chosenLevel = levelSelect;
         typeSelection = chosenLevel.ToString();
         Debug.Log(typeSelection);
         
